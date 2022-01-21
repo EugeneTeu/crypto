f8e576b333ef8d87b68670ed2266cf195f7989ce
eugene teu <eugeneteu@gmail.com>
2022-01-21 13:39:22 +0800

    stream mulitple orderbooks

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ from datetime import datetime
 
 BTC_SYMBOL = "BTCUSD"
 ETH_SYMBOL = "ETHUSD"
+CRV_SYMBOL = "CRVUSD"
+ANKR_SYMBOL = "ANKRUSD"
+YFI_SYMBOL = "YFIUSD"
+
+symbols = [BTC_SYMBOL, ETH_SYMBOL, CRV_SYMBOL]
 
 
 def process(orderBooks, lock):
@@ -27,21 +32,35 @@ def process(orderBooks, lock):
             print(e)
 
 
+def getClient(symbol, orderBooks, lock):
+    url = u.WEB_SOCKET_URL + "/marketdata/" + symbol + "?top_of_book=true"
+    client = c.Client(url, symbol,
+                      orderBook=orderBooks, lock=lock)
+    return client
+
+
 def main():
     lock = threading.Lock()
-    orderBooks = {
-        BTC_SYMBOL: {},
-        ETH_SYMBOL: {},
-        "last_update": None
-    }
-    btcUrl = u.WEB_SOCKET_URL + "/marketdata/" + BTC_SYMBOL + "?top_of_book=true"
-    btc = c.Client(btcUrl, BTC_SYMBOL,
-                   orderBook=orderBooks, lock=lock)
-    ethUrl = u.WEB_SOCKET_URL + "/marketdata/" + ETH_SYMBOL + "?top_of_book=true"
-    eth = c.Client(ethUrl, ETH_SYMBOL,
-                   orderBook=orderBooks, lock=lock)
-    btc.start()
-    eth.start()
+    orderBooks = {}
+    for symbol in symbols:
+        orderBooks[symbol] = {}
+    orderBooks["last_update"] = None
+    for key, value in orderBooks.items():
+        if key != 'last_update':
+            client = getClient(key, orderBooks, lock)
+            client.start()
+    # orderBooks = {
+    #     BTC_SYMBOL: {},
+    #     ETH_SYMBOL: {},
+    #     CRV_SYMBOL: {},
+    #     "last_update": None
+    # }
+    # btc = getClient(BTC_SYMBOL, orderBooks, lock)
+    # eth = getClient(ETH_SYMBOL, orderBooks, lock)
+    # crv = getClient(CRV_SYMBOL, orderBooks, lock)
+    # btc.start()
+    # eth.start()
+    # crv.start()
     process(orderBooks, lock)
 
 
