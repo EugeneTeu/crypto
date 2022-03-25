e9f3fda5a5fc5a5c28a60f83d526913849cbe6b5
eugene teu <eugeneteu@gmail.com>
2022-03-26 00:42:13 +0800

    depreciate dark forest, end of an era

--- polygon/unicorn/enterForest.py
+++ polygon/unicorn/enterForest.py
@@ -9,16 +9,16 @@ from src.unicorn.DarkForestWeb3Client import DarkForestWeb3Client
 from src.clients import unicornNFTClient
 from src.logger.txLogger import txLogger, logTx
 
-darkForestContractAddress = Web3.toChecksumAddress(
-    DarkForestWeb3Client.contractAddress)
-unicorns = unicornNFTClient.getUnicornStatus()
-if not unicorns:
-    txLogger.info("No unicorns found. are they in the dark forest?")
-    exit(1)
+# darkForestContractAddress = Web3.toChecksumAddress(
+#     DarkForestWeb3Client.contractAddress)
+# unicorns = unicornNFTClient.getUnicornStatus()
+# if not unicorns:
+#     txLogger.info("No unicorns found. are they in the dark forest?")
+#     exit(1)
 
-for uni in unicorns:
-    tokenId = uni['tokenId']
-    txHash = unicornNFTClient.stakeUnicorns(darkForestContractAddress, tokenId)
-    txLogger.info(txHash)
-    txReceipt = unicornNFTClient.getTransactionReceipt(txHash)
-    logTx(txReceipt)
+# for uni in unicorns:
+#     tokenId = uni['tokenId']
+#     txHash = unicornNFTClient.stakeUnicorns(darkForestContractAddress, tokenId)
+#     txLogger.info(txHash)
+#     txReceipt = unicornNFTClient.getTransactionReceipt(txHash)
+#     logTx(txReceipt)
--- polygon/unicorn/exitForest.py
+++ polygon/unicorn/exitForest.py
@@ -6,15 +6,15 @@ usage: python3 -m unicorn.exitForest
 from src.clients import darkForestClient
 from src.logger.txLogger import txLogger, logTx
 
-unicorns = darkForestClient.getUnicornStatus()
+# unicorns = darkForestClient.getUnicornStatus()
 
-for uni in unicorns:
-    tokenId = uni['tokenId']
-    canUnstake = uni["canUnstake"]
-    if canUnstake == True:
-        txHash = darkForestClient.unStakeUnicorns(tokenId)
-        txLogger.info(txHash)
-        txReceipt = darkForestClient.getTransactionReceipt(txHash)
-        logTx(txReceipt)
-    else:
-        txLogger.info(str(tokenId) + " cannot unstake right now")
+# for uni in unicorns:
+#     tokenId = uni['tokenId']
+#     canUnstake = uni["canUnstake"]
+#     if canUnstake == True:
+#         txHash = darkForestClient.unStakeUnicorns(tokenId)
+#         txLogger.info(txHash)
+#         txReceipt = darkForestClient.getTransactionReceipt(txHash)
+#         logTx(txReceipt)
+#     else:
+#         txLogger.info(str(tokenId) + " cannot unstake right now")
